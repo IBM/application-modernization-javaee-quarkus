0e1d2ec2ab15a8371908aa64e69a0757283cbef9
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-03 08:12:43 +0100

    trying to convert to open liberty

---
+++ scripts-docker/build-and-run-open-liberty.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+root_folder=$(cd $(dirname $0); cd ..; pwd)
+
+exec 3>&1
+
+function _out() {
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function setup() {
+  echo "Run sh scripts-docker/run-monolith-db2.sh first"
+  echo "curl http://localhost:9086/CustomerOrderServicesWeb/jaxrs/Category"
+  
+  cd ${root_folder}/monolith-open-liberty/CustomerOrderServicesProject
+  mvn clean package
+
+  cd ${root_folder}/monolith-open-liberty/
+  docker build -t storefront-backend-open .  
+  
+  docker run -d -p 9086:9080 --name storefront-backend-open storefront-backend-open  
+
+  docker network connect store-front-network storefront-backend-open
+}
+
+setup
