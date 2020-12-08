c27e4e4e6a8a9035cb0f393b89f2cc5b4c415fd3
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-08 09:24:41 +0100

    trying to find right OL config location

--- scripts-docker/build-ol-backend-and-run-catalog.sh
+++ scripts-docker/build-ol-backend-and-run-catalog.sh
@@ -12,14 +12,20 @@ function setup() {
   echo "Run sh scripts-docker/run-monolith-db2.sh first"
   echo "Open http://localhost/CustomerOrderServicesWeb"
   
+  docker rm -f storefront-frontend
+  docker rm -f storefront-backend
+  docker rm -f proxy-nginx 
+  docker rm -f storefront-monolith
+  docker rm -f storefront-catalog
+  docker rm -f storefront-backend-open
+
   cd ${root_folder}/monolith-open-liberty/CustomerOrderServicesProject
   mvn clean package
-
   cd ${root_folder}/monolith-open-liberty/
   docker build -t storefront-backend-open .
 
   cd ${root_folder}/scripts-docker
-  docker-compose -f docker-compose-splitted-frontend-open.yml up
+  docker-compose -f docker-compose-catalog.yml up
 }
 
 setup
