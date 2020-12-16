c8a14894b2b4f2325cacb3136a7c4491f6e70a0d
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-16 19:41:49 +0100

    fix

--- scripts/install-was-dependencies.sh
+++ scripts/install-was-dependencies.sh
@@ -19,25 +19,32 @@ function setup() {
   rm monolith-websphere-90/resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar
   rm monolith-websphere-90/resources/db2/db2jcc4.jar
   rm monolith-websphere-90/resources/db2/db2jcc_license_cu.jar
+  rm monolith-websphere-liberty/resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar
+  rm monolith-websphere-liberty/resources/db2/db2jcc4.jar
+  rm monolith-websphere-liberty/resources/db2/db2jcc_license_cu.jar
   rm monolith-websphere-90/lib/db2jcc4.jar
    
   cd ${root_folder}
+  rm -rf temp
   mkdir temp
   cd temp
   git clone https://github.com/ibm-cloud-architecture/cloudpak-for-applications.git
   cd cloudpak-for-applications
   git checkout -q was90 
   
-  cp supporting-assets/sessions/db2/db2jcc_license_cu.jar monolith-websphere-90/supporting-assets/sessions/db2/db2jcc_license_cu.jar
-  cp supporting-assets/sessions/db2/db2jcc4.jar monolith-websphere-90/supporting-assets/sessions/db2/db2jcc4.jar
-  cp lib/db2jcc_license_cu.jar monolith-websphere-90/lib/db2jcc_license_cu.jar
-  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/dbunit-2.2.jar monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/dbunit-2.2.jar
-  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/junit.jar monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/junit.jar
-  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/junitee.jar monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/junitee.jar
-  cp resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar monolith-websphere-90/resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar
-  cp resources/db2/db2jcc4.jar monolith-websphere-90/resources/db2/db2jcc4.jar
-  cp resources/db2/db2jcc_license_cu.jar monolith-websphere-90/resources/db2/db2jcc_license_cu.jar
-  cp lib/db2jcc4.jar monolith-websphere-90/lib/db2jcc4.jar
+  cp supporting-assets/sessions/db2/db2jcc_license_cu.jar ${root_folder}/monolith-websphere-90/supporting-assets/sessions/db2/db2jcc_license_cu.jar
+  cp supporting-assets/sessions/db2/db2jcc4.jar ${root_folder}/monolith-websphere-90/supporting-assets/sessions/db2/db2jcc4.jar
+  cp lib/db2jcc_license_cu.jar ${root_folder}/monolith-websphere-90/lib/db2jcc_license_cu.jar
+  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/dbunit-2.2.jar ${root_folder}/monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/dbunit-2.2.jar
+  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/junit.jar ${root_folder}/monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/junit.jar
+  cp CustomerOrderServicesTest/WebContent/WEB-INF/lib/junitee.jar ${root_folder}/monolith-websphere-90/CustomerOrderServicesTest/WebContent/WEB-INF/lib/junitee.jar
+  cp resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar ${root_folder}/monolith-websphere-90/resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar
+  cp resources/db2/db2jcc4.jar ${root_folder}/monolith-websphere-90/resources/db2/db2jcc4.jar
+  cp resources/db2/db2jcc_license_cu.jar ${root_folder}/monolith-websphere-90/resources/db2/db2jcc_license_cu.jar
+  cp resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar ${root_folder}/monolith-websphere-liberty/resources/jmx_exporter/jmx_prometheus_javaagent-0.11.0.jar
+  cp resources/db2/db2jcc4.jar ${root_folder}/monolith-websphere-liberty/resources/db2/db2jcc4.jar
+  cp resources/db2/db2jcc_license_cu.jar ${root_folder}/monolith-websphere-liberty/resources/db2/db2jcc_license_cu.jar
+  cp lib/db2jcc4.jar ${root_folder}/monolith-websphere-90/lib/db2jcc4.jar
 
   cd ${root_folder}
   rm -r temp
