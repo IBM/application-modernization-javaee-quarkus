683c374f2e80878207eb545b34c5bdfb206dd9d9
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-17 16:38:52 +0100

    enable cors in Quarkus

--- frontend-single-spa/catalog/src/components/Home.vue
+++ frontend-single-spa/catalog/src/components/Home.vue
@@ -3,6 +3,42 @@
     <h1 style="text-align: center;">Catalog</h1>
   </div>
 </template>
+
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      apiUrlProducts:
+        "http://localhost/CustomerOrderServicesWeb/jaxrs/Product/?categoryId=2",
+      apiUrlCategories:
+        "http://localhost/CustomerOrderServicesWeb/jaxrs/Category",
+      products: [],
+      categories: [],
+      loading: false,
+      error: "",
+    };
+  },
+  created() {
+    this.readProducts();
+    //this.readCategories();
+  },
+  methods: {
+    readProducts() {
+      fetch(apiUrlProducts)
+        .then((r) => r.json())
+        .then((json) => {
+          //this.products = json;
+          console.log(json);
+        });
+    },
+    readCategories() {
+      fetch(this.apiUrlCategories, { mode: "no-cors" })
+        .then((r) => r.json())
+        .then((json) => {
+          //this.categories = json;
+          console.log(json.message);
+        });
+    },
+  },
+};
 </script>
