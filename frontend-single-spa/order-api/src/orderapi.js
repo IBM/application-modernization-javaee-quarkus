4ed737f9787e641e7f441a233ea97a8cb4efe134
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-22 08:56:54 +0100

    add product to order

--- frontend-single-spa/order-api/src/orderapi.js
+++ frontend-single-spa/order-api/src/orderapi.js
@@ -11,8 +11,35 @@ let OrderAPI = {
         console.log("Topic: " + message.topic) 
         console.log("CommandId: " + message.commandId) 
         console.log("ProductId: " + message.payload.productId) 
+        this.addProductToOrder(message.payload.productId, message.commandId)
       }
     })
+  },
+
+  addProductToOrder(productId, commandId) {
+    let addProductToOrderUrl = "http://localhost/CustomerOrderServicesWeb/jaxrs/Customer/OpenOrder/LineItem"
+    let body = {
+      "quantity":1,
+      "productId":productId
+    }
+    const requestOptions = {
+      method: "POST",
+      headers: { 
+        "Content-Type": "application/json", 
+        "Accept": "*/*",
+        "If-Match": 4
+      },
+      body: JSON.stringify(body)
+    }
+    fetch(addProductToOrderUrl, requestOptions)
+      .then(response => response.json())
+      .then((json) => {
+        console.log("Product has been addded to order")
+      })
+      .catch((error) => {
+        console.log("Product has NOT been addded to order")
+        console.error(error);
+      });
   }
 }
 
