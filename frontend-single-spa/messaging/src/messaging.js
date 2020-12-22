54484b6c6ff72da955e0ab5fd519a6cedf700078
Niklas Heidloff <niklas_heidloff@de.ibm.com>
2020-12-22 09:30:50 +0100

    invoke messaging from orderapi

--- frontend-single-spa/messaging/src/messaging.js
+++ frontend-single-spa/messaging/src/messaging.js
@@ -2,23 +2,34 @@ import { Observable, Subject } from 'rxjs';
 import { OrderAPI } from "@vue-app-mod/order-api";
 
 const TOPIC_COMMAND_ADD_ITEM = "TOPIC_COMMAND_ADD_ITEM"
+const TOPIC_COMMAND_RESPONSE_ADD_ITEM = "TOPIC_COMMAND_RESPONSE_ADD_ITEM"
+const TOPIC_EVENT_AMOUNT_LINE_ITEMS_CHANGED = "TOPIC_EVENT_AMOUNT_LINE_ITEMS_CHANGED"
 
 export default {
   TOPIC_COMMAND_ADD_ITEM,
+  TOPIC_COMMAND_RESPONSE_ADD_ITEM,
+  TOPIC_EVENT_AMOUNT_LINE_ITEMS_CHANGED,
 
   send(message) {
     const observableForOrderAPI = new Subject()
-    OrderAPI.initialize(observableForOrderAPI)
+    OrderAPI.initialize(this, observableForOrderAPI)
 
     let topic = message.topic
     let commandId = message.commandId
     console.log("messaging - messaging.js - send invoked - topic: " + topic + " commandId: " + commandId)
+    console.log(message)
 
     switch (topic) {
       case TOPIC_COMMAND_ADD_ITEM:
-        observableForOrderAPI.next(message);
-        observableForOrderAPI.complete();
-        break;
+        observableForOrderAPI.next(message)
+        observableForOrderAPI.complete()
+        break
+      case TOPIC_COMMAND_RESPONSE_ADD_ITEM:
+        // to be done
+        break
+      case TOPIC_EVENT_AMOUNT_LINE_ITEMS_CHANGED:
+        // to be done
+        break
     }
   },
 };
--- frontend-single-spa/order-api/src/orderapi.js
+++ frontend-single-spa/order-api/src/orderapi.js
@@ -1,9 +1,13 @@
 import { Observable, Subject } from 'rxjs';
 
+let Messaging
+
 let OrderAPI = {
 
-  initialize(observableFromMessaging) {
+  initialize(messagingUtil, observableFromMessaging) {
     console.log("order-api - orderapi.js - initialize invoked")
+
+    Messaging = messagingUtil
 
     observableFromMessaging.subscribe({
       next: (message) => { 
@@ -35,10 +39,41 @@ let OrderAPI = {
       .then(response => response.json())
       .then((json) => {
         console.log("Product has been addded to order")
+        
+        let amountProducts = json.lineitems.length
+        let amountLineItems = 0
+        json.lineitems.forEach(lineItem => {
+          amountLineItems = amountLineItems + lineItem.quantity;
+        })
+        console.log("Products in shopping cart: " + amountProducts)
+        console.log("Line items in shopping cart: " + amountLineItems)
+        let messageAmountShoppingCartChanged = {
+          topic: Messaging.TOPIC_EVENT_AMOUNT_LINE_ITEMS_CHANGED,
+          payload: {
+            amountLineItems: amountLineItems,
+            amountProducts: amountProducts
+          },
+        };
+        Messaging.send(messageAmountShoppingCartChanged);
+
+        let messageCommandResponseAddItem = {
+          topic: Messaging.TOPIC_COMMAND_RESPONSE_ADD_ITEM,
+          payload: {
+            succesful: true            
+          }
+        };
+        Messaging.send(messageCommandResponseAddItem);
       })
       .catch((error) => {
         console.log("Product has NOT been addded to order")
         console.error(error);
+        let messageCommandResponseAddItem = {
+          topic: Messaging.TOPIC_COMMAND_RESPONSE_ADD_ITEM,
+          payload: {
+            succesful: false            
+          }
+        };
+        Messaging.send(messageCommandResponseAddItem);
       });
   }
 }
