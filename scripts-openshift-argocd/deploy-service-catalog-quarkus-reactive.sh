0141f0f5b1ee0b9e837f928d04ec693503275371
nheidloff <nheidloff@gmail.com>
2021-04-19 18:00:27 +0200

    start with argocd

---
+++ scripts-openshift-argocd/deploy-service-catalog-quarkus-reactive.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+root_folder=$(cd $(dirname $0); cd ..; pwd)
+
+exec 3>&1
+
+function _out() {
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function setup() {
+  _out Configuring Tekton and ArgoCD
+  cd $(root_folder)
+  oc project app-mod-argocd-pipelines > /dev/null 2>&1
+  if [ $? != 0 ]; then 
+      oc new-project app-mod-argocd-pipelines
+  fi
+  oc apply -f scripts-openshift-tekton/ClusterRole.yaml
+  oc create clusterrolebinding routes-and-services-reader \
+  --clusterrole=routes-and-services-reader  \
+  --serviceaccount=app-mod-argocd-pipelines:pipeline
+
+  oc apply -f scripts-openshift-argocd/tekton-git-ssh-secret.yaml
+  oc apply -f scripts-openshift-argocd/serviceaccount.yaml
+
+  oc apply -f scripts-openshift-argocd/argocd-config-map.yaml
+  oc apply -f scripts-openshift-argocd/argocd-rbac-config-map.yaml 
+
+  kubectl create secret -n app-mod-argocd-pipelines generic argocd-env-secret '--from-literal=ARGOCD_AUTH_TOKEN=<token>'
+
+  _out Deploying Tekton tasks
+  oc apply -f scripts-openshift-tekton/application/tasks
+
+  _out Start service-catalog-quarkus-reactive pipeline
+  oc apply -f scripts-openshift-argocd/pipelines/service-catalog-quarkus-reactive.yaml
+  oc apply -f scripts-openshift-argocd/pipelineruns/service-catalog-quarkus-reactive.yaml
+}
+
+setup
+
