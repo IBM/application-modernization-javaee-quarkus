77183a6b09b00b9a43ddb5f9460d0fbcaaf4b38f
nheidloff <nheidloff@gmail.com>
2021-05-04 15:54:07 +0200

    prepare managed postgres

---
+++ scripts-docker/run-catalog-synch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+root_folder=$(cd $(dirname $0); cd ..; pwd)
+
+exec 3>&1
+
+function _out() {
+  echo "$(date +'%F %H:%M:%S') $@"
+}
+
+function setup() {
+  echo "Run sh scripts-docker/run-monolith-db2.sh first"
+  echo "Run sh scripts-docker/run-database-postgres-catalog.sh first"
+  echo "Run sh scripts-docker/run-kafka.sh first"
+  echo "Run sh scripts-docker/build-quarkus-and-run-catalog.sh once first"
+  echo "Open http://localhost/CustomerOrderServicesWeb"
+  
+  cd ${root_folder}
+  sh scripts-docker/stop-services.sh
+  
+  cd ${root_folder}/service-catalog-quarkus-synch
+  mvn package
+  docker build -f Dockerfile.single -t storefront-catalog .
+
+  cd ${root_folder}/scripts-docker
+  docker-compose -f docker-compose-all-quarkus.yml up
+}
+
+setup
